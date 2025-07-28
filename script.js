36d7652fbdf92aacdf3c73bf511dc61df3a515ba
Sujeet17m <sujeet17das@gmail.com>
2025-07-28 12:07:00 +0530

    function types

---
+++ script.js
@@ -0,0 +1,16 @@
+//function statement
+function abcd(){
+    console.log("heyy");
+}
+//function expression
+const abcd2 = function() {
+    console.log("heyy2");
+}
+//anonymous function assigned to a variable
+const abcdAnon = function() {
+    console.log("heyy3");
+};
+//arrow function
+const abcd3 = () => {   
+    console.log("heyy4");
+}
