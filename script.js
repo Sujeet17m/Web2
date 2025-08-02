d010c835b4bc07d061ff770ca32a428e671eb7cc
Sujeet17m <sujeet17das@gmail.com>
2025-08-02 16:27:17 +0530

    dom manipulation

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-//function statement
-function abcd(){
-    console.log("heyy");
-}
-//function expression
-const abcd2 = function() {
-    console.log("heyy2");
-}
-//anonymous function assigned to a variable
-const abcdAnon = function() {
-    console.log("heyy3");
-};
-//arrow function
-const abcd3 = () => {   
-    console.log("heyy4");
-}
-abcd();
-abcd2();
-abcdAnon();
-abcd3();
+// //function statement
+// function abcd(){
+//     console.log("heyy");
+// }
+// //function expression
+// const abcd2 = function() {
+//     console.log("heyy2");
+// }
+// //anonymous function assigned to a variable
+// const abcdAnon = function() {
+//     console.log("heyy3");
+// };
+// //arrow function
+// const abcd3 = () => {   
+//     console.log("heyy4");
+// }
+// abcd();
+// abcd2();
+// abcdAnon();
+// abcd3();
 
+var button = document.querySelector("#btn");
+button.addEventListener("click", function() {
+    console.log("Button clicked!");
+});
