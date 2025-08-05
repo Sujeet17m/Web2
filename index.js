533a05b72a108eb152d8bd5d3842570687b65639
Sujeet17m <sujeet17das@gmail.com>
2025-08-06 04:23:36 +0530

    js 2

--- index.js
+++ index.js
@@ -26,14 +26,32 @@
 
 
 // when the button is clicked, it will change the image source between two images
-let btn = document.querySelector("button");
-let img = document.querySelector("img");
-let img2 = document.querySelector("#i2");   
-let img1 = document.querySelector("#i1");
-btn.addEventListener("click", function() {
-    let src1 = img1.src;
-    let src2 = img2.src;  
-    img1.src = src2;
-    img2.src = src1;
+// let btn = document.querySelector("button");
+// let img1 = document.querySelector("#i1");
+// let img2 = document.querySelector("#i2");
+// btn.addEventListener("click",function(){
+//     let src1 = img1.src;
+//     let src2 = img2.src;
+//     img1.src = src2;
+//     img2.src = src1;
+// });
+
+//Javascript to validate the form and display an error message if the input is invalid
+let inptext = document.querySelector("#i1");
+let inpsub = document.querySelector("#i2");
+let btn = document.querySelector("#sub");
+
+
+btn.addEventListener("click",function(){
+    let text = inptext.value;
+    let sub = inpsub.value;
+    
+    if(text === "" || sub === ""){
+        alert("Please fill all the fields");
+    } else {
+        alert("Form submitted successfully");
+        inptext.value = "";
+        inpsub.value = "";
+    }
 });
 
